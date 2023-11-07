7e32d2c10158b97e010bcf3581ecf317896e83a5
ks0727 <kouske51@icloud.com>
2023-11-07 20:29:35 +0900

    327 dr done

---
+++ ABC/301-350/327/dr.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<(n);i++)
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    vector<int> a(m),b(m);
+    rep(i,m) cin >> a[i];
+    rep(i,m) cin >> b[m];
+    bool bipartite = false;
+    auto dfs = [&](auto dfs,int v,int prev){
+        
+    };
+}
