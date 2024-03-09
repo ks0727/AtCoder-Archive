b87f686fa72af697d148340b1a39a3f74a364ec1
ks0727 <kouske51@icloud.com>
2024-03-09 14:44:48 -0500

    ABC 266 ~e done

--- ABC/251-300/266/er.cpp
+++ ABC/251-300/266/er.cpp
@@ -5,5 +5,14 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    
+    double ans = 0;
+    rep(i,n){
+        double now = 0;
+        for(int j=1;j<=6;j++){
+            now += max(double(j),ans);
+        }
+        ans = now/6;
+    }
+    printf("%.10f\n",ans);
+    return 0;
 }
