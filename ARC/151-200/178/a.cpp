054fae027f1a61f5144d5aadbc1ccf2955661e10
ks0727 <kouske51@icloud.com>
2024-05-21 14:05:33 +0900

    ARC 178 done

--- ARC/151-200/178/a.cpp
+++ ARC/151-200/178/a.cpp
@@ -3,6 +3,18 @@ using namespace std;
 #define rep(i,n) for(int i=0;i<n;i++)
 
 int main(){
-    
+    int n,m;
+    cin >> n >> m;
+    vector<int> a(m);
+    rep(i,m) cin >> a[i];
+    vector<int> is(n);
+    rep(i,n) is[i] = i+1;
+    sort(a.begin(),a.end());
+    if(a[0] == 1 || a[m-1] == n){
+        cout << -1 << endl;
+        return 0;
+    }
+    rep(i,m) swap(is[a[i]-1],is[a[i]]);
+    rep(i,n) cout << is[i] << ' '; cout << endl;
     return 0;
 }
--- ARC/151-200/178/b.cpp
+++ ARC/151-200/178/b.cpp
@@ -1,8 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
 #define rep(i,n) for(int i=0;i<n;i++)
+using mint = modint998244353;
+
+mint sum(mint x){
+    return x*(x+1)/2;
+};
+
+void solve(){
+    int a1,a2,a3; cin >> a1 >> a2 >> a3;
+    if(a3 < max(a1,a2) || a3 > max(a1,a2)+1){
+        cout << 0 << '\n';
+        return;
+    }
+    if(a1 == a2 && a2 == a3){
+        mint res = 1;
+        res = 8*mint(10).pow(a1-1);
+        res = sum(res);
+        cout << res.val() << endl;
+        return;
+    }
+    if(a1 > a2) swap(a1,a2);
+    if(a2 == a3){
+        mint mx = mint(10).pow(a3)-mint(10).pow(a1-1)-mint(10).pow(a2-1);
+        mint mn = mint(10).pow(a3)-mint(10).pow(a1) - mint(10).pow(a2-1);
+        mx = sum(mx); mn = sum(mn);
+        mint res = mx - mn;
+        cout << res.val() << endl;
+        return;
+    }else if(a3 == a2+1){
+        mint mx = mint(10).pow(a1)-1;
+        mint mn = mint(10).pow(a1-1)-1;
+        mx = sum(mx); mn = sum(mn);
+        mint res = mx - mn;
+        cout << res.val() << endl;
+        return;
+    }else{
+        cout << 0 << endl;
+        return;
+    }
+}
 
 int main(){
-    
+    int t;
+    cin >> t;
+    rep(ti,t) solve();
     return 0;
 }
